5a51e2a5ba53c9d146fe1ff32ec3e6b177af7338
GomezCanoDaniel <55300282+GomezCanoDaniel@users.noreply.github.com>
2019-11-29 19:57:13 -0600

    Update final.c

--- final.c
+++ final.c
@@ -9,7 +9,7 @@ struct lista
     int edad;
 };
 
-int main()
+
 {
     struct lista listaPersona[50];
     int edadBuscar,i,opcion,j=0;
@@ -31,42 +31,4 @@ int main()
         gets(listaPersona[i].telefono);
     } 
     
-    do
-    {
-        printf("\n");
-        printf("1. Mostrar todos los nombre\n");
-        printf("2. Buscar personas por edad\n");
-        printf("3. Buscar personas cuya inicial se indique\n");
-        printf("4. Salir\n");
-        printf("\nSeleccione opcion::");
-        scanf("%d",&opcion);
-        switch (opcion)
-        {
-            case 1:
-                for (i=0; i<j; i++)
-                    puts(listaPersona[i].nombre);
-                break;
-            case 2:
-                printf("ingrese edad a buscar::");
-                scanf("%d",&edadBuscar);
-                for (i=0; i<j; i++)
-                {
-                    if (listaPersona[i].edad==edadBuscar)
-                        printf("%s\n",listaPersona[i].nombre);
-                }
-                break;
-            case 3:
-                printf("indique inicial del nombre para buscar::");
-                n=getchar();
-                getchar();
-                for (i=0; i<j; i++)
-                {
-                    if (listaPersona[0].nombre[i]==n)
-                        printf("%s\n",listaPersona[i].nombre);
-                }
-                break;
-        }
-    }
-    while (opcion != 4);   
-    return 0;
-}
+ 
